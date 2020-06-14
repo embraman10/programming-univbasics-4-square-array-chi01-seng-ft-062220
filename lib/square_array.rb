67fd16260db01d1e3497f3f971376ccd80a7f637
Emily Braman <embraman10@gmail.com>
2020-06-14 21:14:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,8 @@
 def square_array(array)
   count=0
+  array= [1, 2, 3]
   while count < array.length do
     array[count] = array[count] * array[count]
     count += 1
   end
 end
-
-numbers = [1, 2, 3]
-
-square_array(numbers)
