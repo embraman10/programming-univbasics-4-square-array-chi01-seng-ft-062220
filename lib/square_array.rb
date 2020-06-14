90e420e0b2fc795a44b2b5e6edf16334b0ee3e3f
Emily Braman <embraman10@gmail.com>
2020-06-14 20:18:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,8 @@
-def square_array(ary)
-  numbers = [1,2,3]
-  ary.each_index { |i| a[i] = ary[i] * ary[i] }
-  numbers
+def square_array(array)
+  arr = [1, 2, 3]
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
