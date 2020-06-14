ef8d8b1ec93ee0bf93f7ff1b8dd74d8b3bbb22bf
Emily Braman <embraman10@gmail.com>
2020-06-14 20:28:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
-  arr = []**2
+  while
+  arr = []
+  array.each { |i| arr << i ** 2 }
   arr
 end
 
-my_arr = [1, 2, 3]
-p square_array(my_arr)
+numbers = [1, 2, 3]
+p square_array(numbers)
+
+new_numbers -[9,10,16,25]
+p square_array(new_numbers) 
